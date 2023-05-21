28f7d90fa2dd64824ed23ce8f84ae6b5e81e9835
Kris <109919530+maleker86@users.noreply.github.com>
2023-05-21 14:45:51 -0700

    Max-Width CSS & gridWidth JS

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
 const container = document.getElementById("container");
-const grid = document.getElementById("grid");
-let gridNumber = 16;
+let gridNumber = 85;
 
-let gridWidth = gridNumber + "px";
-//let gridWidth = gridNumber + "px";
+// container.style.width = ((gridNumber * 10) / (gridNumber / (gridNumber / 2))) + "px";
+container.style.width = (gridNumber * 10) + "px";
 
-grid.style.width = gridWidth;
-console.log(gridWidth);
-
-container.style.width = (gridWidth * gridWidth) + "px";
-console.log(container.style.width);
-
-let pixelWidth = gridNumber;
-
-for (i = 0; i < gridNumber; i++) {
+for (i = 0; i < gridNumber * gridNumber; i++) {
   let pixel = document.createElement("div");
-  pixel.style.cssText = `width: ${pixelWidth}px; height: ${pixelWidth}px; border: 1px solid gray;`;
-  // pixel.style.cssText = "width: 8px; height: 8px; border: 1px solid gray;";
-  grid.append(pixel);
+  pixel.style.cssText = "height: 8px; width: 8px; border: 1px solid black;";
+  container.append(pixel);
   console.log(i);
 }
