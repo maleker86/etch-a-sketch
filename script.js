0cff5e4763f74e24cc5e41f565abdd1cd9de537f
Kris <109919530+maleker86@users.noreply.github.com>
2023-05-21 15:11:40 -0700

    Print Grid; Not Access New ClassNames onMouseOver

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const container = document.getElementById("container");
-let gridNumber = 85;
+let gridNumber = 25;
 
-// container.style.width = ((gridNumber * 10) / (gridNumber / (gridNumber / 2))) + "px";
 container.style.width = (gridNumber * 10) + "px";
 
 for (i = 0; i < gridNumber * gridNumber; i++) {
   let pixel = document.createElement("div");
   pixel.style.cssText = "height: 8px; width: 8px; border: 1px solid black;";
+  pixel.setAttribute('class', 'empty');
+
   container.append(pixel);
-  console.log(i);
+//  console.log(i);
 }
+
+let thisPixel = document.getElementsByClassName("empty");
+thisPixel.addEventListener("mouseover", console.log("Yes?"));
