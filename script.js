befeab7b3ea1665155677cfa5e9117d2fafbdc7d
Kris <109919530+maleker86@users.noreply.github.com>
2023-05-21 14:21:33 -0700

    Create Pixels with a number of GridNumber (Bug)

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const container = document.getElementById("container");
-let gridNumber = 10;
+const grid = document.getElementById("grid");
+let gridNumber = 16;
 
-let containerWidth = gridNumber * 10;
-console.log(containerWidth);
+let gridWidth = gridNumber + "px";
+//let gridWidth = gridNumber + "px";
 
-container.style.width = containerWidth + "px";
+grid.style.width = gridWidth;
+console.log(gridWidth);
 
-for (i = 0; i < gridNumber * gridNumber; i++) {
+container.style.width = (gridWidth * gridWidth) + "px";
+console.log(container.style.width);
+
+let pixelWidth = gridNumber;
+
+for (i = 0; i < gridNumber; i++) {
   let pixel = document.createElement("div");
-  pixel.style.cssText = "width: 8px; height: 8px; border: 1px solid black;";
-  container.append(pixel);
-  //    console.log(i);
+  pixel.style.cssText = `width: ${pixelWidth}px; height: ${pixelWidth}px; border: 1px solid gray;`;
+  // pixel.style.cssText = "width: 8px; height: 8px; border: 1px solid gray;";
+  grid.append(pixel);
+  console.log(i);
 }
