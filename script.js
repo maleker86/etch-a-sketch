d27cbb2c7f6f4a99bfc83a3c4ae67d80a636fd95
Kris <109919530+maleker86@users.noreply.github.com>
2023-05-21 11:35:39 -0700

    Resize Grid; Not In FlexWidth (Bug)

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const container = document.getElementById("container");
-let gridNumber = 16;
+let gridNumber = 10;
+
+let containerWidth = gridNumber * 10;
+console.log(containerWidth);
+
+container.style.width = containerWidth + "px";
 
 for (i = 0; i < gridNumber * gridNumber; i++) {
-    container.style.cssText = `background-color: pink; max-width: calc(${gridNumber} * 10)px`;
-    let pixel = document.createElement('div');
-    pixel.style.cssText = 'width: 8px; height: 8px; border: 1px solid black;';
-    container.append(pixel);
-//    console.log(i);
+  let pixel = document.createElement("div");
+  pixel.style.cssText = "width: 8px; height: 8px; border: 1px solid black;";
+  container.append(pixel);
+  //    console.log(i);
 }
