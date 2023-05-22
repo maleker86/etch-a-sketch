11a59c79ff66a3b9556e30f9161d03d5e41c4ca3
Kris <109919530+maleker86@users.noreply.github.com>
2023-05-21 17:56:13 -0700

    CREATE HOVER EFFECTS (BUG SMASH)

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const container = document.getElementById("container");
-let gridNumber = 25;
+let gridNumber = 32;
 
 container.style.width = (gridNumber * 10) + "px";
 
+// draw as many pixel boxes as gridNumber
 for (i = 0; i < gridNumber * gridNumber; i++) {
   let pixel = document.createElement("div");
   pixel.style.cssText = "height: 8px; width: 8px; border: 1px solid black;";
   pixel.setAttribute('class', 'empty');
 
+  pixel.addEventListener("mouseover", myTest)
+
   container.append(pixel);
 //  console.log(i);
 }
 
-let thisPixel = document.getElementsByClassName("empty");
-thisPixel.addEventListener("mouseover", console.log("Yes?"));
+//get pixels to alter class, but testing it first.
+//let pixels = document.getElementsByClassName("empty");
+
+//pixels.addEventListener("mouseover", myTest);
+
+function myTest() {
+//  console.log("Moused Over");
+  this.removeAttribute('empty');
+  this.setAttribute('class', 'activated'); 
+}
