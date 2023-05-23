9ec15190ecac9a983821e27416a6ccb4b6dca83b
Kris <109919530+maleker86@users.noreply.github.com>
2023-05-22 18:06:43 -0700

    Implement Grid Size Customization

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const container = document.getElementById("container");
-let gridNumber = 24;
+let gridNumber = 4;
 
 let containerWidth = container.offsetWidth;
 
@@ -11,18 +11,47 @@ for (i = 0; i < gridNumber * gridNumber; i++) {
   pixel.style.height = ((containerWidth / gridNumber) - 2) + "px";
   pixel.setAttribute('class', 'empty');
 
-  pixel.addEventListener("mouseover", myTest)
+  pixel.addEventListener("mouseover", pixelHover)
 
   container.append(pixel);
-//  console.log(i);
 }
 
 console.log(containerWidth);
 
-//pixels.addEventListener("mouseover", myTest);
-
-function myTest() {
+function pixelHover() {
   // console.log("Moused Over");
-  this.removeAttribute('empty');
-  this.setAttribute('class', 'activated');
-} 
+  this.removeAttribute("empty");
+  this.setAttribute("class", "activated");
+}
+
+function getNumber() {
+  if (gridNumber) {
+    gridNumber = prompt("Please provide a number", "16");
+  } else {
+    gridNumber = prompt("Please provide a number", "16");
+  }
+  if (isNaN(gridNumber)) {
+    alert("Must input numbers");
+    return false;
+  }
+  if (gridNumber > 100) {
+    alert("Please use a number between 1 and 100");
+    return false;
+  }
+    console.log(`New Number is ${gridNumber}`);
+    container.textContent = "";
+
+  for (i = 0; i < gridNumber * gridNumber; i++) {
+    let pixel = document.createElement("div");
+    pixel.style.cssText = "border: 1px solid black;";
+    pixel.style.width = containerWidth / gridNumber - 2 + "px";
+    pixel.style.height = containerWidth / gridNumber - 2 + "px";
+    pixel.setAttribute("class", "empty");
+
+    pixel.addEventListener("mouseover", pixelHover);
+
+    container.append(pixel);
+
+    // console.log(i);
+  }
+}
