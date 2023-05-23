88ec130277a4a97494c717b6f3790d1a56cb351b
Kris <109919530+maleker86@users.noreply.github.com>
2023-05-22 17:58:13 -0700

    Resize Pixels within Static FlexBox Grid

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const container = document.getElementById("container");
 let gridNumber = 24;
 
-container.style.width = (gridNumber * 10) + "px";
+let containerWidth = container.offsetWidth;
 
 // draw as many pixel boxes as gridNumber
 for (i = 0; i < gridNumber * gridNumber; i++) {
   let pixel = document.createElement("div");
-  pixel.style.cssText = "height: 8px; width: 8px; border: 1px solid black;";
+  pixel.style.cssText = "border: 1px solid black;";
+  pixel.style.width = ((containerWidth / gridNumber) - 2) + "px";
+  pixel.style.height = ((containerWidth / gridNumber) - 2) + "px";
   pixel.setAttribute('class', 'empty');
 
   pixel.addEventListener("mouseover", myTest)
@@ -15,10 +17,12 @@ for (i = 0; i < gridNumber * gridNumber; i++) {
 //  console.log(i);
 }
 
+console.log(containerWidth);
+
 //pixels.addEventListener("mouseover", myTest);
 
 function myTest() {
-//  console.log("Moused Over");
+  // console.log("Moused Over");
   this.removeAttribute('empty');
-  this.setAttribute('class', 'activated'); 
-}
+  this.setAttribute('class', 'activated');
+} 
